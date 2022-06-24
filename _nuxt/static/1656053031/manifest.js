1e204868ab7d3f5834faa622bf38ca9e48169428
Teki <w2495969292@gmail.com>
2022-06-24 15:48:14 +0900

    cahnge name tool2

--- _nuxt/static/1656053031/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Ftool1","\u002F","\u002Ftool2","\u002Ftool3"]})
--- _nuxt/static/1656053031/tool2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flab-tools\u002F_nuxt\u002Fstatic\u002F1656053031",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftool2",config:{_app:{basePath:"\u002Flab-tools\u002F",assetsPath:"\u002Flab-tools\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"214fedb5"}}}}(null));
